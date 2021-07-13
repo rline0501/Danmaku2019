8e5dc84e278f6853f5836305eae6dbb5f7b32b99
rline <crossrun000@gmail.com>
2021-07-13 16:07:27 +0900

    初回

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //移動速度
+    public float moveSpeed = 0.2f;
+
+    private Vector3 pos;
+
+    void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal") * moveSpeed;
+
+        float moveV = Input.GetAxis("Vertical") * moveSpeed;
+
+        transform.Translate(moveH, 0, moveV);
+
+        MoveClamp();
+
+    }
+
+/// <summary>
+/// 移動範囲
+/// </summary>
+    void MoveClamp()
+    {
+        pos = transform.position;
+
+        //ぞれぞれ座標を入力した最小値と最大値の間に制限する
+        pos.x = Mathf.Clamp(pos.x, -10, 10);
+        pos.z = Mathf.Clamp(pos.z, -10, 10);
+
+        transform.position = pos;
+    }
+}
